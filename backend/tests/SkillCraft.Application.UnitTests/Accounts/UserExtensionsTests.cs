969363cb4f88133b78cd69b3cb4a3fa2d4318518
Francis Pion <francispion@hotmail.com>
2024-07-30 09:57:28 -0400

    todos

--- backend/tests/SkillCraft.Application.UnitTests/Accounts/UserExtensionsTests.cs
+++ backend/tests/SkillCraft.Application.UnitTests/Accounts/UserExtensionsTests.cs
@@ -10,16 +10,6 @@ public class UserExtensionsTests
 {
   private readonly Faker _faker = new();
 
-  //[Fact(DisplayName = "CompleteProfile: it should set the correct custom attribute on the payload.")]
-  //public void CompleteProfile_it_should_set_the_correct_custom_attribute_on_the_payload()
-  //{
-  //  UpdateUserPayload payload = new();
-  //  Assert.Empty(payload.CustomAttributes);
-
-  //  payload.CompleteProfile();
-  //  Assert.Contains(payload.CustomAttributes, c => c.Key == "ProfileCompletedOn" && DateTime.TryParse(c.Value, out _));
-  //} // TODO(fpion): CompleteProfile
-
   [Fact(DisplayName = "GetCustomAttribute: it should return the found value.")]
   public void GetCustomAttribute_it_should_return_the_found_value()
   {
@@ -73,38 +63,6 @@ public class UserExtensionsTests
     user.CustomAttributes.Add(new(nameof(MultiFactorAuthenticationMode), mfaMode.ToString()));
     Assert.Equal(mfaMode, user.GetMultiFactorAuthenticationMode());
   }
-
-  //[Theory(DisplayName = "GetPhone: it should return the correct phone from token claims.")]
-  //[InlineData(null, "(514) 845-4636", null, "+15148454636", false)]
-  //[InlineData("CA", "(514) 845-4636", "98772", "+15148454636", true)]
-  //[InlineData("CA", "(514) 845-4636", "98772", "", false)]
-  //[InlineData("CA", "          ", null, "+15148454636", false)]
-  //public void GetPhone_it_should_return_the_correct_phone_from_token_claims(string? countryCode, string number, string? extension, string e164Formatted, bool isVerified)
-  //{
-  //  ValidatedToken validatedToken = new();
-  //  if (countryCode != null)
-  //  {
-  //    validatedToken.Claims.Add(new TokenClaim(ClaimNames.PhoneCountryCode, countryCode));
-  //  }
-  //  validatedToken.Claims.Add(new TokenClaim(ClaimNames.PhoneNumberRaw, number));
-  //  validatedToken.Claims.Add(new TokenClaim(Rfc7519ClaimNames.PhoneNumber, extension == null ? e164Formatted : $"{e164Formatted};ext={extension}"));
-  //  validatedToken.Claims.Add(new TokenClaim(Rfc7519ClaimNames.IsPhoneVerified, isVerified.ToString().ToLower(), ClaimValueTypes.Boolean));
-
-  //  Phone? phone = validatedToken.GetPhone();
-  //  if (string.IsNullOrWhiteSpace(number) || string.IsNullOrWhiteSpace(e164Formatted))
-  //  {
-  //    Assert.Null(phone);
-  //  }
-  //  else
-  //  {
-  //    Assert.NotNull(phone);
-  //    Assert.Equal(countryCode, phone.CountryCode);
-  //    Assert.Equal(number, phone.Number);
-  //    Assert.Equal(extension, phone.Extension);
-  //    Assert.Equal(e164Formatted, phone.E164Formatted);
-  //    Assert.Equal(isVerified, phone.IsVerified);
-  //  }
-  //} // TODO(fpion): GetPhone
 
   [Fact(DisplayName = "GetProfileCompleted: it should throw ArgumentException when the user profile is not completed.")]
   public void GetProfileCompleted_it_should_throw_ArgumentException_when_the_user_profile_is_not_completed()
@@ -187,85 +145,6 @@ public class UserExtensionsTests
     user.CustomAttributes.Add(new("ProfileCompletedOn", DateTime.UtcNow.ToISOString()));
     Assert.True(user.IsProfileCompleted());
   }
-
-  //[Theory(DisplayName = "SetMultiFactorAuthenticationMode: it should the correct custom attribute on the payload")]
-  //[InlineData(MultiFactorAuthenticationMode.Email)]
-  //public void SetMultiFactorAuthenticationMode_it_should_the_correct_custom_attribute_on_the_payload(MultiFactorAuthenticationMode mfaMode)
-  //{
-  //  UpdateUserPayload payload = new();
-  //  Assert.Empty(payload.CustomAttributes);
-
-  //  payload.SetMultiFactorAuthenticationMode(mfaMode);
-  //  Assert.Contains(payload.CustomAttributes, c => c.Key == nameof(MultiFactorAuthenticationMode) && c.Value == mfaMode.ToString());
-  //} // TODO(fpion): SetMultiFactorAuthenticationMode
-
-  //[Fact(DisplayName = "ToEmailPayload: it should return the correct payload.")]
-  //public void ToEmailPayload_it_should_return_the_correct_payload()
-  //{
-  //  Email email = new(_faker.Person.Email);
-  //  EmailPayload payload = email.ToEmailPayload();
-  //  Assert.Equal(email.Address, payload.Address);
-  //  Assert.Equal(email.IsVerified, payload.IsVerified);
-
-  //  payload = email.ToEmailPayload(isVerified: true);
-  //  Assert.Equal(email.Address, payload.Address);
-  //  Assert.True(payload.IsVerified);
-  //} // TODO(fpion): ToEmailPayload
-
-  //[Fact(DisplayName = "ToPhonePayload: it should return the correct payload.")]
-  //public void ToPhonePayload_it_should_return_the_correct_payload()
-  //{
-  //  Phone phone = new("CA", "(514) 845-4636", "123456", "+15148454636");
-  //  PhonePayload payload = phone.ToPhonePayload();
-  //  Assert.Equal(phone.CountryCode, payload.CountryCode);
-  //  Assert.Equal(phone.Number, payload.Number);
-  //  Assert.Equal(phone.Extension, payload.Extension);
-  //  Assert.Equal(phone.IsVerified, payload.IsVerified);
-
-  //  payload = phone.ToPhonePayload(isVerified: true);
-  //  Assert.Equal(phone.CountryCode, payload.CountryCode);
-  //  Assert.Equal(phone.Number, payload.Number);
-  //  Assert.Equal(phone.Extension, payload.Extension);
-  //  Assert.True(payload.IsVerified);
-  //} // TODO(fpion): ToPhonePayload
-
-  //[Fact(DisplayName = "ToUpdateUserPayload: it should return the correct payload.")]
-  //public void ToUpdateUserPayload_it_should_return_the_correct_payload()
-  //{
-  //  SaveProfilePayload payload = new(_faker.Person.FirstName, _faker.Person.LastName, _faker.Locale, "America/Montreal")
-  //  {
-  //    MiddleName = null,
-  //    Birthdate = _faker.Person.DateOfBirth,
-  //    Gender = _faker.Person.Gender.ToString().ToLower()
-  //  };
-  //  UpdateUserPayload update = payload.ToUpdateUserPayload();
-  //  Assert.Equal(payload.FirstName, update.FirstName?.Value);
-  //  Assert.NotNull(update.MiddleName);
-  //  Assert.Equal(payload.MiddleName, update.MiddleName.Value);
-  //  Assert.Equal(payload.LastName, update.LastName?.Value);
-  //  Assert.Equal(payload.Birthdate, update.Birthdate?.Value);
-  //  Assert.Equal(payload.Gender, update.Gender?.Value);
-  //  Assert.Equal(payload.Locale, update.Locale?.Value);
-  //  Assert.Equal(payload.TimeZone, update.TimeZone?.Value);
-  //} // TODO(fpion): ToUpdateUserPayload
-
-  //[Theory(DisplayName = "ToPhone: it should return the correct phone.")]
-  //[InlineData("CA", "(514) 845-4636", "+15148454636")]
-  //[InlineData("", "(514) 845-4636", "+15148454636")]
-  //[InlineData("  ", "  (514) 845-4636  ", "+15148454636")]
-  //[InlineData(null, "(514) 845-4636", "+15148454636")]
-  //public void ToPhone_it_should_return_the_correct_phone(string? countryCode, string number, string e164Formatted)
-  //{
-  //  AccountPhone phone = new(number, countryCode);
-  //  Phone result = phone.ToPhone();
-  //  Assert.Equal(phone.CountryCode?.CleanTrim(), result.CountryCode);
-  //  Assert.Equal(phone.Number.Trim(), result.Number);
-  //  Assert.Null(result.Extension);
-  //  Assert.Equal(e164Formatted, result.E164Formatted);
-  //  Assert.False(result.IsVerified);
-  //  Assert.Null(result.VerifiedBy);
-  //  Assert.Null(result.VerifiedOn);
-  //} // TODO(fpion): ToPhone
 
   [Fact(DisplayName = "ToUserProfile: it should return the correct user profile.")]
   public void ToUserProfile_it_should_return_the_correct_user_profile()
