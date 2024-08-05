838a78021b95d9b6ce86b9e2e32cd199a56d4f94
Francis Pion <francispion@hotmail.com>
2024-08-04 21:35:29 -0400

    todos

--- backend/src/SkillCraft.Application/Actors/IActorService.cs
+++ backend/src/SkillCraft.Application/Actors/IActorService.cs
@@ -8,4 +8,5 @@ public interface IActorService
 {
   Task<IReadOnlyCollection<Actor>> FindAsync(IEnumerable<ActorId> ids, CancellationToken cancellationToken = default);
   Task SaveAsync(User user, CancellationToken cancellationToken = default);
+  // TODO(fpion): more SaveAsync in account command handlers
 }
