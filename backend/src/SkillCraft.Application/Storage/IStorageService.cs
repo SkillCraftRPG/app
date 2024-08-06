ac92135021b07f2e91acb514de952d6b18212486
Francis Pion <francispion@hotmail.com>
2024-08-06 01:05:55 -0400

    todo

--- backend/src/SkillCraft.Application/Storage/IStorageService.cs
+++ backend/src/SkillCraft.Application/Storage/IStorageService.cs
@@ -5,7 +5,6 @@ namespace SkillCraft.Application.Storage;
 public interface IStorageService
 {
   Task EnsureAvailableAsync(IStoredEntity entity, CancellationToken cancellationToken = default);
-  Task EnsureAvailableAsync(IStoredEntity entity, int previousSize, CancellationToken cancellationToken = default);
-
+  Task EnsureAvailableAsync(IStoredEntity entity, int previousSize, CancellationToken cancellationToken = default); // TODO(fpion): remove this method, previous can be found in the storage aggregate
   Task UpdateAsync(IStoredEntity entity, CancellationToken cancellationToken = default);
 }
