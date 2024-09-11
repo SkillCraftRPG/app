a441106cf8fafdedc8b5c77edbef40b22b21a3a1
Francis Pion <francispion@hotmail.com>
2024-09-10 22:25:06 -0400

    Contracts.

---
+++ backend/src/SkillCraft.Contracts/Worlds/CreateWorldPayload.cs
@@ -0,0 +1,17 @@
+﻿namespace SkillCraft.Contracts.Worlds;
+
+public record CreateWorldPayload
+{
+  public string Slug { get; set; }
+  public string? Name { get; set; }
+  public string? Description { get; set; }
+
+  public CreateWorldPayload() : this(string.Empty)
+  {
+  }
+
+  public CreateWorldPayload(string slug)
+  {
+    Slug = slug;
+  }
+}
---
+++ backend/src/SkillCraft.Contracts/Worlds/WorldModel.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.Portal.Contracts;
+using Logitar.Portal.Contracts.Actors;
+
+namespace SkillCraft.Contracts.Worlds;
+
+public class WorldModel : Aggregate
+{
+  public string Slug { get; set; }
+  public string? Name { get; set; }
+  public string? Description { get; set; }
+
+  public Actor Owner { get; set; }
+
+  public WorldModel() : this(new Actor(), string.Empty)
+  {
+  }
+
+  public WorldModel(Actor owner, string slug)
+  {
+    Owner = owner;
+
+    Slug = slug;
+  }
+
+  public override string ToString() => $"{Name ?? Slug} | {base.ToString()}";
+}
