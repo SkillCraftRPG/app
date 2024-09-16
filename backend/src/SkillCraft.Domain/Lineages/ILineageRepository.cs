639fe7dbe1d95afc471265ffeb1fd44d812a22f8
Francis Pion <francispion@hotmail.com>
2024-09-16 14:18:31 -0400

    domain

---
+++ backend/src/SkillCraft.Domain/Lineages/ILineageRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SkillCraft.Domain.Lineages;
+
+public interface ILineageRepository
+{
+  Task<IReadOnlyCollection<Lineage>> LoadAsync(CancellationToken cancellationToken = default);
+
+  Task<Lineage?> LoadAsync(LineageId id, CancellationToken cancellationToken = default);
+  Task<Lineage?> LoadAsync(LineageId id, long? version, CancellationToken cancellationToken = default);
+
+  Task SaveAsync(Lineage lineage, CancellationToken cancellationToken = default);
+  Task SaveAsync(IEnumerable<Lineage> lineages, CancellationToken cancellationToken = default);
+}
---
+++ backend/src/SkillCraft.Domain/Lineages/LineageId.cs
@@ -0,0 +1,33 @@
+﻿using Logitar.EventSourcing;
+
+namespace SkillCraft.Domain.Lineages;
+
+public readonly struct LineageId
+{
+  public AggregateId AggregateId { get; }
+  public string Value => AggregateId.Value;
+
+  public LineageId(AggregateId actorId)
+  {
+    AggregateId = actorId;
+  }
+  public LineageId(Guid value)
+  {
+    AggregateId = new(value);
+  }
+  public LineageId(string value)
+  {
+    AggregateId = new(value);
+  }
+
+  public static LineageId NewId() => new(AggregateId.NewId());
+
+  public Guid ToGuid() => AggregateId.ToGuid();
+
+  public static bool operator ==(LineageId left, LineageId right) => left.Equals(right);
+  public static bool operator !=(LineageId left, LineageId right) => !left.Equals(right);
+
+  public override bool Equals([NotNullWhen(true)] object? obj) => obj is LineageId userId && userId.Value == Value;
+  public override int GetHashCode() => Value.GetHashCode();
+  public override string ToString() => Value;
+}
