98a565f062102b5c416c76d24bf3837664e6eb3b
Francis Pion <francispion@hotmail.com>
2024-09-16 22:48:50 -0400

    Implemented Aspect GraphQL schema.

---
+++ backend/src/SkillCraft.GraphQL/Aspects/SearchAspectsPayloadGraphType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using SkillCraft.Contracts.Aspects;
+using SkillCraft.GraphQL.Search;
+
+namespace SkillCraft.GraphQL.Aspects;
+
+internal class SearchAspectsPayloadGraphType : SearchPayloadInputGraphType<SearchAspectsPayload>
+{
+  public SearchAspectsPayloadGraphType() : base()
+  {
+    Field(x => x.Sort, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<AspectSortOptionGraphType>>>))
+      .DefaultValue([])
+      .Description("The sort parameters of the search.");
+  }
+}
