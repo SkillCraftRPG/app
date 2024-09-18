1e0fdcfc15d397c2e397e60931da602d8ac37db3
Francis Pion <francispion@hotmail.com>
2024-09-17 22:13:18 -0400

    Implemented Customization GraphQL schema.

---
+++ backend/src/SkillCraft.GraphQL/Customizations/CustomizationQueries.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using SkillCraft.Application.Customizations.Queries;
+using SkillCraft.Contracts.Customizations;
+
+namespace SkillCraft.GraphQL.Customizations;
+
+internal static class CustomizationQueries
+{
+  public static void Register(RootQuery root)
+  {
+    root.Field<CustomizationGraphType>("customization")
+      .Authorize()
+      .Description("Retrieves a single customization.")
+      .Argument<NonNullGraphType<IdGraphType>>(name: "id", description: "The unique identifier of the customization.")
+      .ResolveAsync(async context => await context.ExecuteQueryAsync(new ReadCustomizationQuery(
+        context.GetArgument<Guid>("id")
+      ), context.CancellationToken));
+
+    root.Field<NonNullGraphType<CustomizationSearchResultsGraphType>>("customizations")
+      .Authorize()
+      .Description("Searches a list of customizations.")
+      .Argument<NonNullGraphType<SearchCustomizationsPayloadGraphType>>(name: "payload", description: "The parameters to apply to the search.")
+      .ResolveAsync(async context => await context.ExecuteQueryAsync(new SearchCustomizationsQuery(
+        context.GetArgument<SearchCustomizationsPayload>("payload")
+      ), context.CancellationToken));
+  }
+}
---
+++ backend/src/SkillCraft.GraphQL/Customizations/CustomizationSortGraphType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using SkillCraft.Contracts.Customizations;
+
+namespace SkillCraft.GraphQL.Customizations;
+
+internal class CustomizationSortGraphType : EnumerationGraphType<CustomizationSort>
+{
+  public CustomizationSortGraphType()
+  {
+    Name = nameof(CustomizationSort);
+    Description = "Represents the available customization fields for sorting.";
+
+    AddValue(CustomizationSort.Name, "The customizations will be sorted by their display name.");
+    AddValue(CustomizationSort.UpdatedOn, "The customizations will be sorted by their latest update date and time.");
+  }
+  private void AddValue(CustomizationSort value, string description)
+  {
+    Add(value.ToString(), value, description);
+  }
+}
