8370358ef65eb95d9395d947fc05b465fd76935d
Francis Pion <francispion@hotmail.com>
2024-09-17 23:05:39 -0400

    Implemented Language GraphQL schema. (#43)

---
+++ backend/src/SkillCraft.GraphQL/Languages/SearchLanguagesPayloadGraphType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using SkillCraft.Contracts.Languages;
+using SkillCraft.GraphQL.Search;
+
+namespace SkillCraft.GraphQL.Languages;
+
+internal class SearchLanguagesPayloadGraphType : SearchPayloadInputGraphType<SearchLanguagesPayload>
+{
+  public SearchLanguagesPayloadGraphType() : base()
+  {
+    Field(x => x.Sort, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<LanguageSortOptionGraphType>>>))
+      .DefaultValue([])
+      .Description("The sort parameters of the search.");
+  }
+}
