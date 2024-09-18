38db63559d8b33c6060ec59bfd90fe59c5f1a70d
Francis Pion <francispion@hotmail.com>
2024-09-17 20:54:41 -0400

    Implemented Education GraphQL schema. (#40)

---
+++ backend/src/SkillCraft.GraphQL/Educations/EducationGraphType.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using SkillCraft.Contracts.Educations;
+using SkillCraft.GraphQL.Worlds;
+
+namespace SkillCraft.GraphQL.Educations;
+
+internal class EducationGraphType : AggregateGraphType<EducationModel>
+{
+  public EducationGraphType() : base("Represents a character education.")
+  {
+    Field(x => x.Name)
+      .Description("The display name of the education.");
+    Field(x => x.Description)
+      .Description("The description of the education.");
+
+    Field(x => x.Skill, type: typeof(SkillGraphType))
+      .Description("The skill talent required by this education.");
+    Field(x => x.WealthMultiplier)
+      .Description("The starting wealth multiplier of characters having this education.");
+
+    Field(x => x.World, type: typeof(NonNullGraphType<WorldGraphType>))
+      .Description("The world in which the education resides.");
+  }
+}
---
+++ backend/src/SkillCraft.GraphQL/Educations/EducationSearchResultsGraphType.cs
@@ -0,0 +1,11 @@
+﻿using SkillCraft.Contracts.Educations;
+using SkillCraft.GraphQL.Search;
+
+namespace SkillCraft.GraphQL.Educations;
+
+internal class EducationSearchResultsGraphType : SearchResultsGraphType<EducationGraphType, EducationModel>
+{
+  public EducationSearchResultsGraphType() : base("EducationSearchResults", "Represents the results of an education search.")
+  {
+  }
+}
