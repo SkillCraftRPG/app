a90f41b0702979a8e277e6c271f7aec6f4943213
Francis Pion <francispion@hotmail.com>
2024-09-17 22:23:46 -0400

    Implemented Personality GraphQL schema.

---
+++ backend/src/SkillCraft.GraphQL/Personalities/PersonalityQueries.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using SkillCraft.Application.Personalities.Queries;
+using SkillCraft.Contracts.Personalities;
+
+namespace SkillCraft.GraphQL.Personalities;
+
+internal static class PersonalityQueries
+{
+  public static void Register(RootQuery root)
+  {
+    root.Field<PersonalityGraphType>("personality")
+      .Authorize()
+      .Description("Retrieves a single personality.")
+      .Argument<NonNullGraphType<IdGraphType>>(name: "id", description: "The unique identifier of the personality.")
+      .ResolveAsync(async context => await context.ExecuteQueryAsync(new ReadPersonalityQuery(
+        context.GetArgument<Guid>("id")
+      ), context.CancellationToken));
+
+    root.Field<NonNullGraphType<PersonalitySearchResultsGraphType>>("personalities")
+      .Authorize()
+      .Description("Searches a list of personalities.")
+      .Argument<NonNullGraphType<SearchPersonalitiesPayloadGraphType>>(name: "payload", description: "The parameters to apply to the search.")
+      .ResolveAsync(async context => await context.ExecuteQueryAsync(new SearchPersonalitiesQuery(
+        context.GetArgument<SearchPersonalitiesPayload>("payload")
+      ), context.CancellationToken));
+  }
+}
---
+++ backend/src/SkillCraft.GraphQL/Personalities/SearchPersonalitiesPayloadGraphType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using SkillCraft.Contracts.Personalities;
+using SkillCraft.GraphQL.Search;
+
+namespace SkillCraft.GraphQL.Personalities;
+
+internal class SearchPersonalitiesPayloadGraphType : SearchPayloadInputGraphType<SearchPersonalitiesPayload>
+{
+  public SearchPersonalitiesPayloadGraphType() : base()
+  {
+    Field(x => x.Sort, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<PersonalitySortOptionGraphType>>>))
+      .DefaultValue([])
+      .Description("The sort parameters of the search.");
+  }
+}
