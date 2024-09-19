809a0160dea59a4659d9a8c08ba418097181562e
Francis Pion <francispion@hotmail.com>
2024-09-19 11:04:54 -0400

    contracts & validators

---
+++ backend/src/SkillCraft.Application/Talents/Validators/CreateTalentValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using SkillCraft.Contracts.Talents;
+using SkillCraft.Domain.Validators;
+
+namespace SkillCraft.Application.Talents.Validators;
+
+internal class CreateTalentValidator : AbstractValidator<CreateTalentPayload>
+{
+  public CreateTalentValidator()
+  {
+    RuleFor(x => x.Tier).Tier();
+
+    RuleFor(x => x.Name).Name();
+    When(x => !string.IsNullOrWhiteSpace(x.Description), () => RuleFor(x => x.Description!).Description());
+
+    When(x => x.RequiredTalentId.HasValue, () => RuleFor(x => x.RequiredTalentId!.Value).NotEmpty());
+  }
+}
---
+++ backend/src/SkillCraft.Application/Talents/Validators/UpdateTalentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SkillCraft.Contracts.Talents;
+using SkillCraft.Domain.Validators;
+
+namespace SkillCraft.Application.Talents.Validators;
+
+internal class UpdateTalentValidator : AbstractValidator<UpdateTalentPayload>
+{
+  public UpdateTalentValidator()
+  {
+    When(x => !string.IsNullOrWhiteSpace(x.Name), () => RuleFor(x => x.Name!).Name());
+    When(x => !string.IsNullOrWhiteSpace(x.Description?.Value), () => RuleFor(x => x.Description!.Value!).Description());
+
+    When(x => x.RequiredTalentId?.Value != null, () => RuleFor(x => x.RequiredTalentId!.Value!.Value).NotEmpty());
+  }
+}
