874cc7aa3da0b64235f4dd2199a324e78c2da11d
Francis Pion <francispion@hotmail.com>
2024-09-19 11:12:34 -0400

    repo

---
+++ backend/src/SkillCraft.Domain/Talents/ITalentRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SkillCraft.Domain.Talents;
+
+public interface ITalentRepository
+{
+  Task<IReadOnlyCollection<Talent>> LoadAsync(CancellationToken cancellationToken = default);
+
+  Task<Talent?> LoadAsync(TalentId id, CancellationToken cancellationToken = default);
+  Task<Talent?> LoadAsync(TalentId id, long? version, CancellationToken cancellationToken = default);
+
+  Task SaveAsync(Talent talent, CancellationToken cancellationToken = default);
+  Task SaveAsync(IEnumerable<Talent> talents, CancellationToken cancellationToken = default);
+}
