6ef7deed4476651ff6ed5af84c313780a5595ba2
Francis Pion <francispion@hotmail.com>
2024-09-20 13:13:35 -0400

    repo

--- backend/src/SkillCraft.Domain/Customizations/ICustomizationRepository.cs
+++ backend/src/SkillCraft.Domain/Customizations/ICustomizationRepository.cs
@@ -7,6 +7,8 @@ public interface ICustomizationRepository
   Task<Customization?> LoadAsync(CustomizationId id, CancellationToken cancellationToken = default);
   Task<Customization?> LoadAsync(CustomizationId id, long? version, CancellationToken cancellationToken = default);
 
+  Task<IReadOnlyCollection<Customization>> LoadAsync(IEnumerable<CustomizationId> ids, CancellationToken cancellationToken = default);
+
   Task SaveAsync(Customization customization, CancellationToken cancellationToken = default);
   Task SaveAsync(IEnumerable<Customization> customizations, CancellationToken cancellationToken = default);
 }
