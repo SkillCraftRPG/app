512baeaf7c661f0d76a3c3c998d0befa48338fba
Francis Pion <francispion@hotmail.com>
2024-09-22 23:58:30 -0400

    domain

---
+++ backend/src/SkillCraft.Domain/Comments/Comment.cs
@@ -0,0 +1,31 @@
+﻿using Logitar.EventSourcing;
+using MediatR;
+
+namespace SkillCraft.Domain.Comments;
+
+public class Comment : AggregateRoot
+{
+  public new CommentId Id => new(base.Id);
+
+  private Text? _text = null;
+  public Text Text => _text ?? throw new InvalidOperationException($"The {nameof(Text)} has not been initialized yet.");
+
+  public Comment(Text text, UserId userId, CommentId? id = null) : base(id?.AggregateId)
+  {
+    Raise(new CreatedEvent(text), userId.ActorId);
+  }
+  protected virtual void Apply(CreatedEvent @event)
+  {
+    _text = @event.Text;
+  }
+
+  public class CreatedEvent : DomainEvent, INotification
+  {
+    public Text Text { get; }
+
+    public CreatedEvent(Text text)
+    {
+      Text = text;
+    }
+  }
+}
---
+++ backend/src/SkillCraft.Domain/Comments/Text.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SkillCraft.Domain.Comments.Validators;
+
+namespace SkillCraft.Domain.Comments;
+
+public record Text
+{
+  public const int MaximumLength = byte.MaxValue;
+
+  public string Value { get; }
+
+  public Text(string value)
+  {
+    Value = value.Trim();
+    new TextValidator().ValidateAndThrow(this);
+  }
+}
