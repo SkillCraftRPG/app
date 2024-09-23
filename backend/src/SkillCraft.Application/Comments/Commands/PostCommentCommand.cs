0e4fd9a8d270dcb9de46efc250205fcd54e344c5
Francis Pion <francispion@hotmail.com>
2024-09-23 18:46:51 -0400

    refac

--- backend/src/SkillCraft.Application/Comments/Commands/PostCommentCommand.cs
+++ backend/src/SkillCraft.Application/Comments/Commands/PostCommentCommand.cs
@@ -36,13 +36,14 @@ internal class PostCommentCommandHandler : IRequestHandler<PostCommentCommand, C
     PostCommentPayload payload = command.Payload;
     new PostCommentValidator().ValidateAndThrow(payload);
 
-    EntityKey key = new(command.EntityType, command.EntityId);
-    WorldId? worldId = await _worldQuerier.FindIdAsync(key, cancellationToken);
+    EntityKey entity = new(command.EntityType, command.EntityId);
+    WorldId? worldId = await _worldQuerier.FindIdAsync(entity, cancellationToken);
     if (!worldId.HasValue)
     {
       return null;
     }
-    if (key.Type == EntityType.World)
+
+    if (entity.Type == EntityType.World)
     {
       World world = await _worldRepository.LoadAsync(worldId.Value, cancellationToken)
         ?? throw new InvalidOperationException($"The world 'Id={worldId}' could not be found.");
@@ -50,11 +51,11 @@ internal class PostCommentCommandHandler : IRequestHandler<PostCommentCommand, C
     }
     else
     {
-      EntityMetadata entity = new(worldId.Value, key, size: 1);
-      await _permissionService.EnsureCanCommentAsync(command, entity, cancellationToken);
+      EntityMetadata entityMetadata = new(worldId.Value, entity, size: 1);
+      await _permissionService.EnsureCanCommentAsync(command, entityMetadata, cancellationToken);
     }
 
-    Comment comment = Comment.Post(worldId.Value, key, new Text(payload.Text), command.GetUserId());
+    Comment comment = Comment.Post(worldId.Value, entity, new Text(payload.Text), command.GetUserId());
 
     // TODO(fpion): save; 402 --> no more storage
 
