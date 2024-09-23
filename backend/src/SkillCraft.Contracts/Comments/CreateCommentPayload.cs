779d061f00695f45356567ac2004bebad70454f8
Francis Pion <francispion@hotmail.com>
2024-09-22 23:57:55 -0400

    contracts

---
+++ backend/src/SkillCraft.Contracts/Comments/CreateCommentPayload.cs
@@ -0,0 +1,15 @@
+﻿namespace SkillCraft.Contracts.Comments;
+
+public record CreateCommentPayload
+{
+  public string Text { get; set; }
+
+  public CreateCommentPayload() : this(string.Empty)
+  {
+  }
+
+  public CreateCommentPayload(string text)
+  {
+    Text = text;
+  }
+}
