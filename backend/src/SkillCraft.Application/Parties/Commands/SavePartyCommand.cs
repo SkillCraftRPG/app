6682250d7e604e8e0f861914ce7c6cd005c68ee4
Francis Pion <francispion@hotmail.com>
2024-09-23 00:30:19 -0400

    application

---
+++ backend/src/SkillCraft.Application/Parties/Commands/SavePartyCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SkillCraft.Application.Storages;
+using SkillCraft.Domain.Parties;
+
+namespace SkillCraft.Application.Parties.Commands;
+
+internal record SavePartyCommand(Party Party) : IRequest;
+
+internal class SavePartyCommandHandler : IRequestHandler<SavePartyCommand>
+{
+  private readonly IPartyRepository _partyRepository;
+  private readonly IStorageService _storageService;
+
+  public SavePartyCommandHandler(IPartyRepository partyRepository, IStorageService storageService)
+  {
+    _partyRepository = partyRepository;
+    _storageService = storageService;
+  }
+
+  public async Task Handle(SavePartyCommand command, CancellationToken cancellationToken)
+  {
+    Party party = command.Party;
+
+    EntityMetadata entity = party.GetMetadata();
+    await _storageService.EnsureAvailableAsync(entity, cancellationToken);
+
+    await _partyRepository.SaveAsync(party, cancellationToken);
+
+    await _storageService.UpdateAsync(entity, cancellationToken);
+  }
+}
---
+++ backend/src/SkillCraft.Application/Parties/Validators/CreatePartyValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SkillCraft.Contracts.Parties;
+using SkillCraft.Domain.Validators;
+
+namespace SkillCraft.Application.Parties.Validators;
+
+internal class CreatePartyValidator : AbstractValidator<CreatePartyPayload>
+{
+  public CreatePartyValidator()
+  {
+    RuleFor(x => x.Name).Name();
+    When(x => !string.IsNullOrWhiteSpace(x.Description), () => RuleFor(x => x.Description!).Description());
+  }
+}
