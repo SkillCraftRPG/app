b28e2156e37401b3796f11fedeb9947ee044546b
Francis Pion <francispion@hotmail.com>
2024-09-26 18:32:06 -0400

    Added Personality filters & sort options.

--- backend/src/SkillCraft.Contracts/Personalities/SearchPersonalitiesPayload.cs
+++ backend/src/SkillCraft.Contracts/Personalities/SearchPersonalitiesPayload.cs
@@ -4,5 +4,8 @@ namespace SkillCraft.Contracts.Personalities;
 
 public record SearchPersonalitiesPayload : SearchPayload
 {
+  public Attribute? Attribute { get; set; }
+  public Guid? GiftId { get; set; }
+
   public new List<PersonalitySortOption> Sort { get; set; } = [];
 }
--- backend/src/SkillCraft/Models/Personalities/SearchPersonalitiesParameters.cs
+++ backend/src/SkillCraft/Models/Personalities/SearchPersonalitiesParameters.cs
@@ -1,14 +1,26 @@
 ﻿using Logitar.Portal.Contracts.Search;
+using Microsoft.AspNetCore.Mvc;
 using SkillCraft.Contracts.Personalities;
 using SkillCraft.Models.Search;
+using Attribute = SkillCraft.Contracts.Attribute;
 
 namespace SkillCraft.Models.Personalities;
 
 public record SearchPersonalitiesParameters : SearchParameters
 {
+  [FromQuery(Name = "attribute")]
+  public Attribute? Attribute { get; set; }
+
+  [FromQuery(Name = "gift")]
+  public Guid? GiftId { get; set; }
+
   public SearchPersonalitiesPayload ToPayload()
   {
-    SearchPersonalitiesPayload payload = new();
+    SearchPersonalitiesPayload payload = new()
+    {
+      Attribute = Attribute,
+      GiftId = GiftId
+    };
     Fill(payload);
 
     foreach (SortOption sort in ((SearchPayload)payload).Sort)
