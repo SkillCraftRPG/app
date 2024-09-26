da8b1effd7e47d3ef28418f3b02154d836e8e024
Francis Pion <francispion@hotmail.com>
2024-09-26 18:34:27 -0400

    Added Party sort options.

--- backend/src/SkillCraft.GraphQL/Parties/PartySortGraphType.cs
+++ backend/src/SkillCraft.GraphQL/Parties/PartySortGraphType.cs
@@ -10,6 +10,7 @@ internal class PartySortGraphType : EnumerationGraphType<PartySort>
     Name = nameof(PartySort);
     Description = "Represents the available party fields for sorting.";
 
+    AddValue(PartySort.CreatedOn, "The parties will be sorted by their creation date and time.");
     AddValue(PartySort.Name, "The parties will be sorted by their display name.");
     AddValue(PartySort.UpdatedOn, "The parties will be sorted by their latest update date and time.");
   }
