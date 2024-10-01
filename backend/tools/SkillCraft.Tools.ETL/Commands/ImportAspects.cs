c8c5b76bf03cdfa6fec0995651e4851c1b3f3e87
Francis Pion <francispion@hotmail.com>
2024-09-30 23:13:12 -0400

    Implemented ETL base. (#83)

---
+++ backend/tools/SkillCraft.Tools.ETL/Commands/ImportAspects.cs
@@ -0,0 +1,96 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using MediatR;
+using SkillCraft.Application.Aspects.Commands;
+using SkillCraft.Contracts;
+using SkillCraft.Contracts.Aspects;
+using System.Globalization;
+using Attribute = SkillCraft.Contracts.Attribute;
+
+namespace SkillCraft.Tools.ETL.Commands;
+
+internal record ImportAspects : INotification;
+
+internal class ImportAspectsHandler : INotificationHandler<ImportAspects>
+{
+  private readonly IApiClient _client;
+
+  public ImportAspectsHandler(IApiClient client)
+  {
+    _client = client;
+  }
+
+  public async Task Handle(ImportAspects _, CancellationToken cancellationToken)
+  {
+    IReadOnlyCollection<Aspect> aspects = Extract();
+    IReadOnlyCollection<SaveAspectCommand> commands = Transform(aspects);
+    await LoadAsync(commands, cancellationToken);
+  }
+
+  private static IReadOnlyCollection<Aspect> Extract()
+  {
+    StreamReader reader = new("data/aspects.csv");
+    CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+    IEnumerable<Aspect> records = csv.GetRecords<Aspect>();
+    return records.ToArray().AsReadOnly();
+  }
+
+  private static IReadOnlyCollection<SaveAspectCommand> Transform(IEnumerable<Aspect> aspects) => aspects.Select(aspect =>
+  {
+    SaveAspectPayload payload = new(aspect.Name)
+    {
+      Description = aspect.Description,
+      Attributes = new AttributeSelectionModel
+      {
+        Mandatory1 = aspect.MandatoryAttribute1,
+        Mandatory2 = aspect.MandatoryAttribute2,
+        Optional1 = aspect.OptionalAttribute1,
+        Optional2 = aspect.OptionalAttribute2
+      },
+      Skills = new SkillsModel
+      {
+        Discounted1 = aspect.DiscountedSkill1,
+        Discounted2 = aspect.DiscountedSkill2
+      }
+    };
+    return new SaveAspectCommand(aspect.Id, payload, Version: null);
+  }).ToArray().AsReadOnly();
+
+  private async Task LoadAsync(IEnumerable<SaveAspectCommand> commands, CancellationToken cancellationToken)
+  {
+    foreach (SaveAspectCommand command in commands)
+    {
+      await _client.SaveAspectAsync(command, cancellationToken);
+    }
+  }
+}
+
+internal record Aspect
+{
+  [Name("id")]
+  public Guid Id { get; set; }
+
+  [Name("name")]
+  public string Name { get; set; } = string.Empty;
+
+  [Name("description")]
+  public string? Description { get; set; }
+
+  [Name("attributes.mandatory1")]
+  public Attribute? MandatoryAttribute1 { get; set; }
+
+  [Name("attributes.mandatory2")]
+  public Attribute? MandatoryAttribute2 { get; set; }
+
+  [Name("attributes.optional1")]
+  public Attribute? OptionalAttribute1 { get; set; }
+
+  [Name("attributes.optional2")]
+  public Attribute? OptionalAttribute2 { get; set; }
+
+  [Name("skills.discounted1")]
+  public Skill? DiscountedSkill1 { get; set; }
+
+  [Name("skills.discounted2")]
+  public Skill? DiscountedSkill2 { get; set; }
+}
---
+++ backend/tools/SkillCraft.Tools.ETL/EtlWorker.cs
@@ -0,0 +1,74 @@
+﻿using Logitar.Net.Http;
+using MediatR;
+using SkillCraft.Tools.ETL.Commands;
+
+namespace SkillCraft.Tools.ETL;
+
+public class EtlWorker : BackgroundService
+{
+  private const string GenericErrorMessage = "An unhanded exception occurred.";
+
+  private readonly IHostApplicationLifetime _hostApplicationLifetime;
+  private readonly ILogger<EtlWorker> _logger;
+  private readonly IServiceProvider _serviceProvider;
+
+  private LogLevel _result = LogLevel.Information; // NOTE(fpion): "Information" means success.
+
+  public EtlWorker(IHostApplicationLifetime hostApplicationLifetime, ILogger<EtlWorker> logger, IServiceProvider serviceProvider)
+  {
+    _hostApplicationLifetime = hostApplicationLifetime;
+    _logger = logger;
+    _serviceProvider = serviceProvider;
+  }
+
+  protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+  {
+    Stopwatch chrono = Stopwatch.StartNew();
+    _logger.LogInformation("Worker executing at {Timestamp}.", DateTimeOffset.Now);
+
+    using IServiceScope scope = _serviceProvider.CreateScope();
+    IPublisher publisher = scope.ServiceProvider.GetRequiredService<IPublisher>();
+
+    await Task.Delay(5000, cancellationToken);
+
+    try
+    {
+      await publisher.Publish(new ImportWorlds(), cancellationToken);
+      await publisher.Publish(new ImportParties(), cancellationToken);
+      await publisher.Publish(new ImportAspects(), cancellationToken);
+    }
+    catch (Exception exception)
+    {
+      _logger.LogError(exception, GenericErrorMessage);
+      _result = LogLevel.Error;
+
+      if (exception is HttpFailureException<JsonApiResult> jsonException)
+      {
+        _logger.LogError("{JsonContent}", jsonException.Result.JsonContent);
+      }
+
+      Environment.ExitCode = exception.HResult;
+    }
+    finally
+    {
+      chrono.Stop();
+
+      long seconds = chrono.ElapsedMilliseconds / 1000;
+      string secondText = seconds <= 1 ? "second" : "seconds";
+      switch (_result)
+      {
+        case LogLevel.Error:
+          _logger.LogError("ETL failed after {Elapsed}ms ({Seconds} {SecondText}).", chrono.ElapsedMilliseconds, seconds, secondText);
+          break;
+        case LogLevel.Warning:
+          _logger.LogWarning("ETL completed with warnings in {Elapsed}ms ({Seconds} {SecondText}).", chrono.ElapsedMilliseconds, seconds, secondText);
+          break;
+        default:
+          _logger.LogInformation("ETL succeeded in {Elapsed}ms ({Seconds} {SecondText}).", chrono.ElapsedMilliseconds, seconds, secondText);
+          break;
+      }
+
+      _hostApplicationLifetime.StopApplication();
+    }
+  }
+}
