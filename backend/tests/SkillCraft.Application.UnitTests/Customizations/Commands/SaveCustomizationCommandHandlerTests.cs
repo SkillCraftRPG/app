8acb424a5c0651c676e3bdcc861c6b3bf49c1ff3
Francis Pion <francispion@hotmail.com>
2024-10-02 23:29:57 -0400

    SaveCustomizationCommandHandlerTests

---
+++ backend/tests/SkillCraft.Application.UnitTests/Customizations/Commands/SaveCustomizationCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using SkillCraft.Application.Storages;
+using SkillCraft.Contracts.Customizations;
+using SkillCraft.Domain;
+using SkillCraft.Domain.Customizations;
+
+namespace SkillCraft.Application.Customizations.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class SaveCustomizationCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+
+  private readonly Mock<ICustomizationRepository> _customizationRepository = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly SaveCustomizationCommandHandler _handler;
+
+  public SaveCustomizationCommandHandlerTests()
+  {
+    _handler = new(_customizationRepository.Object, _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should save the customization.")]
+  public async Task It_should_save_the_customization()
+  {
+    WorldMock world = new();
+    Customization customization = new(world.Id, CustomizationType.Gift, new Name("Mystérieux"), world.OwnerId);
+
+    SaveCustomizationCommand command = new(customization);
+    await _handler.Handle(command, _cancellationToken);
+
+    _customizationRepository.Verify(x => x.SaveAsync(customization, _cancellationToken), Times.Once);
+
+    EntityMetadata entity = customization.GetMetadata();
+    _storageService.Verify(x => x.EnsureAvailableAsync(entity, _cancellationToken), Times.Once);
+    _storageService.Verify(x => x.UpdateAsync(entity, _cancellationToken), Times.Once);
+  }
+}
