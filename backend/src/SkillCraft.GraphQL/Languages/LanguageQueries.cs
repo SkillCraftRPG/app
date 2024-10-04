03d9695a406ea6ae5b6e048bbd1496dc15fd74c3
Francis Pion <francispion@hotmail.com>
2024-10-03 23:06:55 -0400

    Implemented scripts query.

--- backend/src/SkillCraft.GraphQL/Languages/LanguageQueries.cs
+++ backend/src/SkillCraft.GraphQL/Languages/LanguageQueries.cs
@@ -24,5 +24,10 @@ internal static class LanguageQueries
       .ResolveAsync(async context => await context.ExecuteQueryAsync(new SearchLanguagesQuery(
         context.GetArgument<SearchLanguagesPayload>("payload")
       ), context.CancellationToken));
+
+    root.Field<NonNullGraphType<ScriptSearchResultsGraphType>>("scripts")
+      .Authorize()
+      .Description("TODO")
+      .ResolveAsync(async context => await context.ExecuteQueryAsync(new SearchScriptsQuery(), context.CancellationToken));
   }
 }
---
+++ backend/src/SkillCraft.GraphQL/Languages/ScriptSearchResultsGraphType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using Logitar.Portal.Contracts.Search;
+
+namespace SkillCraft.GraphQL.Languages;
+
+internal class ScriptSearchResultsGraphType : ObjectGraphType<SearchResults<string>>
+{
+  public ScriptSearchResultsGraphType()
+  {
+    Name = "ScriptSearchResults";
+    Description = "Represents the results of a script search.";
+
+    Field(x => x.Items, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<StringGraphType>>>))
+      .Description("The list of matching scripts.");
+    Field(x => x.Total)
+      .Description("The total number of matching scripts.");
+  }
+}
