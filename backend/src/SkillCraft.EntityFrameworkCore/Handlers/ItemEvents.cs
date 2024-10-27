05ae73ecebace636100237ba3046ab4eb23dfa1a
Francis Pion <francispion@hotmail.com>
2024-10-27 17:58:28 -0400

    ItemEvents

--- backend/src/SkillCraft.EntityFrameworkCore/Handlers/ItemEvents.cs
+++ backend/src/SkillCraft.EntityFrameworkCore/Handlers/ItemEvents.cs
@@ -7,6 +7,48 @@ namespace SkillCraft.EntityFrameworkCore.Handlers;
 
 internal static class ItemEvents
 {
+  public class ConsumablePropertiesUpdatedEventHandler : INotificationHandler<Item.ConsumablePropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public ConsumablePropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.ConsumablePropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
+  public class ContainerPropertiesUpdatedEventHandler : INotificationHandler<Item.ContainerPropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public ContainerPropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.ContainerPropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
   public class CreatedEventHandler : INotificationHandler<Item.CreatedEvent>
   {
     private readonly SkillCraftContext _context;
@@ -36,6 +78,90 @@ internal static class ItemEvents
     }
   }
 
+  public class DevicePropertiesUpdatedEventHandler : INotificationHandler<Item.DevicePropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public DevicePropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.DevicePropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
+  public class EquipmentPropertiesUpdatedEventHandler : INotificationHandler<Item.EquipmentPropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public EquipmentPropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.EquipmentPropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
+  public class MiscellaneousPropertiesUpdatedEventHandler : INotificationHandler<Item.MiscellaneousPropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public MiscellaneousPropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.MiscellaneousPropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
+  public class MoneyPropertiesUpdatedEventHandler : INotificationHandler<Item.MoneyPropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public MoneyPropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.MoneyPropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
   public class UpdatedEventHandler : INotificationHandler<Item.UpdatedEvent>
   {
     private readonly SkillCraftContext _context;
@@ -52,6 +178,27 @@ internal static class ItemEvents
         ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
 
       item.Update(@event);
+
+      await _context.SaveChangesAsync(cancellationToken);
+    }
+  }
+
+  public class WeaponPropertiesUpdatedEventHandler : INotificationHandler<Item.WeaponPropertiesUpdatedEvent>
+  {
+    private readonly SkillCraftContext _context;
+
+    public WeaponPropertiesUpdatedEventHandler(SkillCraftContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Handle(Item.WeaponPropertiesUpdatedEvent @event, CancellationToken cancellationToken)
+    {
+      ItemEntity item = await _context.Items
+        .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken)
+        ?? throw new InvalidOperationException($"The item entity 'AggregateId={@event.AggregateId}' could not be found.");
+
+      item.SetProperties(@event);
 
       await _context.SaveChangesAsync(cancellationToken);
     }
