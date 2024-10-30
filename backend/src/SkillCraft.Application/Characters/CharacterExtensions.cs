487f6ef7f12762175738ee8f7ef722f06bd23034
Francis Pion <francispion@hotmail.com>
2024-10-30 15:54:04 -0400

    CharacterExtensioCharacterExtensionsns

--- backend/src/SkillCraft.Application/Characters/CharacterExtensions.cs
+++ backend/src/SkillCraft.Application/Characters/CharacterExtensions.cs
@@ -6,11 +6,7 @@ namespace SkillCraft.Application.Characters;
 
 internal static class CharacterExtensions
 {
-  public static EntityMetadata GetMetadata(this Character character)
-  {
-    long size = character.Name.Size + (character.Player?.Size ?? 0)
-      + 4 /* LineageId */ + 8 /* Height */ + 8 /* Weight */ + 4 /* Age */;
-    // TODO(fpion): complete
-    return new EntityMetadata(character.WorldId, new EntityKey(EntityType.Character, character.EntityId), size);
-  }
+  public static EntityMetadata GetMetadata(this Character character) => new(character.WorldId, new EntityKey(EntityType.Character, character.EntityId), character.CalculateSize());
+
+  private static long CalculateSize(this Character character) => character.Name.Size + (character.Player?.Size ?? 0); // TODO(fpion): calculate character size
 }
