4c1ae378dd384aca865e100f81750616cc4c4258
Francis Pion <francispion@hotmail.com>
2024-10-30 15:38:37 -0400

    CustomizationExtensions

--- backend/src/SkillCraft.Application/Customizations/CustomizationExtensions.cs
+++ backend/src/SkillCraft.Application/Customizations/CustomizationExtensions.cs
@@ -1,23 +1,12 @@
 ﻿using SkillCraft.Contracts;
-using SkillCraft.Contracts.Customizations;
 using SkillCraft.Domain;
 using SkillCraft.Domain.Customizations;
-using SkillCraft.Domain.Worlds;
 
 namespace SkillCraft.Application.Customizations;
 
 internal static class CustomizationExtensions
 {
-  private const EntityType Type = EntityType.Customization;
+  public static EntityMetadata GetMetadata(this Customization customization) => new(customization.WorldId, new EntityKey(EntityType.Customization, customization.EntityId), customization.CalculateSize());
 
-  public static EntityMetadata GetMetadata(this Customization customization)
-  {
-    long size = 4 + customization.Name.Size + (customization.Description?.Size ?? 0);
-    return new EntityMetadata(customization.WorldId, new EntityKey(Type, customization.EntityId), size);
-  }
-  public static EntityMetadata GetMetadata(this CustomizationModel customization)
-  {
-    long size = 4 + customization.Name.Length + (customization.Description?.Length ?? 0);
-    return new EntityMetadata(new WorldId(customization.World.Id), new EntityKey(Type, customization.Id), size);
-  }
+  private static long CalculateSize(this Customization customization) => 4 /* Type */ + customization.Name.Size + (customization.Description?.Size ?? 0);
 }
