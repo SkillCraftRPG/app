d415f2688bfc664399706a31a6424b4e3f6298d6
Francis Pion <francispion@hotmail.com>
2024-10-30 15:39:52 -0400

    LanguageExtensions

--- backend/src/SkillCraft.Application/Languages/LanguageExtensions.cs
+++ backend/src/SkillCraft.Application/Languages/LanguageExtensions.cs
@@ -1,23 +1,13 @@
 ﻿using SkillCraft.Contracts;
-using SkillCraft.Contracts.Languages;
 using SkillCraft.Domain;
 using SkillCraft.Domain.Languages;
-using SkillCraft.Domain.Worlds;
 
 namespace SkillCraft.Application.Languages;
 
 internal static class LanguageExtensions
 {
-  private const EntityType Type = EntityType.Language;
+  public static EntityMetadata GetMetadata(this Language language) => new(language.WorldId, new EntityKey(EntityType.Language, language.EntityId), language.CalculateSize());
 
-  public static EntityMetadata GetMetadata(this Language language)
-  {
-    long size = language.Name.Size + (language.Description?.Size ?? 0) + (language.Script?.Size ?? 0) + (language.TypicalSpeakers?.Size ?? 0);
-    return new EntityMetadata(language.WorldId, new EntityKey(Type, language.EntityId), size);
-  }
-  public static EntityMetadata GetMetadata(this LanguageModel language)
-  {
-    long size = language.Name.Length + (language.Description?.Length ?? 0) + (language.Script?.Length ?? 0) + (language.TypicalSpeakers?.Length ?? 0);
-    return new EntityMetadata(new WorldId(language.World.Id), new EntityKey(Type, language.Id), size);
-  }
+  private static long CalculateSize(this Language language) => language.Name.Size + (language.Description?.Size ?? 0)
+    + (language.Script?.Size ?? 0) + (language.TypicalSpeakers?.Size ?? 0);
 }
