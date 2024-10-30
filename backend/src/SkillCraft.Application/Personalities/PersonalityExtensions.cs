8ab4853efa7635d1178cf0d335752ac44ad644a2
Francis Pion <francispion@hotmail.com>
2024-10-30 15:52:17 -0400

    castes, educations, personalities, comments

--- backend/src/SkillCraft.Application/Personalities/PersonalityExtensions.cs
+++ backend/src/SkillCraft.Application/Personalities/PersonalityExtensions.cs
@@ -1,23 +1,13 @@
 ﻿using SkillCraft.Contracts;
-using SkillCraft.Contracts.Personalities;
 using SkillCraft.Domain;
 using SkillCraft.Domain.Personalities;
-using SkillCraft.Domain.Worlds;
 
 namespace SkillCraft.Application.Personalities;
 
 internal static class PersonalityExtensions
 {
-  private const EntityType Type = EntityType.Personality;
+  public static EntityMetadata GetMetadata(this Personality personality) => new(personality.WorldId, new EntityKey(EntityType.Personality, personality.EntityId), personality.CalculateSize());
 
-  public static EntityMetadata GetMetadata(this Personality personality)
-  {
-    long size = personality.Name.Size + (personality.Description?.Size ?? 0) + 4 + 4;
-    return new EntityMetadata(personality.WorldId, new EntityKey(Type, personality.EntityId), size);
-  }
-  public static EntityMetadata GetMetadata(this PersonalityModel personality)
-  {
-    long size = personality.Name.Length + (personality.Description?.Length ?? 0) + 4 + 4;
-    return new EntityMetadata(new WorldId(personality.World.Id), new EntityKey(Type, personality.Id), size);
-  }
+  private static long CalculateSize(this Personality personality) => personality.Name.Size + (personality.Description?.Size ?? 0)
+    + 4 /* Attribute */ + 4 /* GiftId */;
 }
--- backend/src/SkillCraft.Domain/Castes/Trait.cs
+++ backend/src/SkillCraft.Domain/Castes/Trait.cs
@@ -1,3 +1,7 @@
 ﻿namespace SkillCraft.Domain.Castes;
 
-public record Trait(Name Name, Description? Description);
+public record Trait(Name Name, Description? Description)
+{
+  [JsonIgnore]
+  public int Size => Name.Size + (Description?.Size ?? 0);
+}
