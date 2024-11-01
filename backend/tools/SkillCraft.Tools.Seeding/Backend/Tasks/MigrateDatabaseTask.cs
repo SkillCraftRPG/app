78bd378e4cc607401e7cc3c9053236fafc0aeecc
Francis Pion <francispion@hotmail.com>
2024-11-01 12:06:33 -0400

    Refactored seeding tasks.

--- backend/tools/SkillCraft.Tools.Seeding/Backend/Tasks/MigrateDatabaseTask.cs
+++ backend/tools/SkillCraft.Tools.Seeding/Backend/Tasks/MigrateDatabaseTask.cs
@@ -1,6 +1,29 @@
-﻿namespace SkillCraft.Tools.Seeding.Backend.Tasks;
+﻿using Logitar.EventSourcing.EntityFrameworkCore.Relational;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkillCraft.EntityFrameworkCore;
+
+namespace SkillCraft.Tools.Seeding.Backend.Tasks;
 
 internal class MigrateDatabaseTask : SeedingTask
 {
   public override string? Description => "Applies database migrations.";
+}
+
+internal class MigrateDatabaseTaskHandler : INotificationHandler<MigrateDatabaseTask>
+{
+  private readonly EventContext _eventContext;
+  private readonly SkillCraftContext _skillCraftContext;
+
+  public MigrateDatabaseTaskHandler(EventContext eventContext, SkillCraftContext skillCraftContext)
+  {
+    _eventContext = eventContext;
+    _skillCraftContext = skillCraftContext;
+  }
+
+  public async Task Handle(MigrateDatabaseTask notification, CancellationToken cancellationToken)
+  {
+    await _eventContext.Database.MigrateAsync(cancellationToken);
+    await _skillCraftContext.Database.MigrateAsync(cancellationToken);
+  }
 }
