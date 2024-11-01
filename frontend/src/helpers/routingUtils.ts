7b8171f5a37fec130a7b9c6bce7c4fce61b8b2e7
Francis Pion <francispion@hotmail.com>
2024-10-31 23:15:16 -0400

    No more todos!

--- frontend/src/helpers/routingUtils.ts
+++ frontend/src/helpers/routingUtils.ts
@@ -2,4 +2,4 @@ export function getWorldSlug(): string | undefined {
   const parts: string[] = window.location.href.split("/");
   const index: number = parts.indexOf("worlds");
   return index < 0 ? undefined : parts[index + 1] || undefined;
-} // TODO(fpion): unit tests
+}
