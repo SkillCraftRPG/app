3375ebad3118bd6b9d38365d89f57ef0986fcbc5
Francis Pion <francispion@hotmail.com>
2024-11-01 14:47:32 -0400

    Implemented Aspect creation.

--- frontend/src/api/aspects.ts
+++ frontend/src/api/aspects.ts
@@ -1,11 +1,16 @@
 import { urlUtils } from "logitar-js";
 
-import type { AspectModel, SearchAspectsPayload } from "@/types/aspects";
+import type { AspectModel, CreateOrReplaceAspectPayload, SearchAspectsPayload } from "@/types/aspects";
 import type { SearchResults } from "@/types/search";
-import { get } from ".";
+import { get, post } from ".";
 
 function createUrlBuilder(id?: string): urlUtils.IUrlBuilder {
   return id ? new urlUtils.UrlBuilder({ path: "/aspects/{id}" }).setParameter("id", id) : new urlUtils.UrlBuilder({ path: "/aspects" });
+}
+
+export async function createAspect(payload: CreateOrReplaceAspectPayload): Promise<AspectModel> {
+  const url: string = createUrlBuilder().buildRelative();
+  return (await post<CreateOrReplaceAspectPayload, AspectModel>(url, payload)).data;
 }
 
 export async function searchAspects(payload: SearchAspectsPayload): Promise<SearchResults<AspectModel>> {
