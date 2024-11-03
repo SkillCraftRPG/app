e5cc652712ae8fdc2f836983852ccb7e8d79caf2
Francis Pion <francispion@hotmail.com>
2024-11-02 21:18:03 -0400

    Implemented Script edit.

---
+++ frontend/src/components/languages/ScriptInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import AppInput from "@/components/shared/AppInput.vue";
+
+defineProps<{
+  modelValue?: string;
+}>();
+
+defineEmits<{
+  (e: "update:model-value", value?: string): void;
+}>();
+</script>
+
+<template>
+  <AppInput
+    floating
+    label="languages.script.label"
+    max="255"
+    :model-value="modelValue"
+    placeholder="languages.script.label"
+    @update:model-value="$emit('update:model-value', $event)"
+  />
+</template>
---
+++ frontend/src/components/languages/ScriptSelect.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { SelectOption } from "logitar-vue3-ui";
+import { computed, onMounted, ref } from "vue";
+
+import AppSelect from "@/components/shared/AppSelect.vue";
+import type { SearchResults } from "@/types/search";
+import { listScripts } from "@/api/languages";
+
+defineProps<{
+  modelValue?: string;
+}>();
+
+const scripts = ref<string[]>([]);
+
+const options = computed<SelectOption[]>(() => scripts.value.map((script) => ({ text: script, value: script })));
+
+const emit = defineEmits<{
+  (e: "error", value: unknown): void;
+  (e: "update:model-value", value?: string): void;
+}>();
+
+onMounted(async () => {
+  try {
+    const results: SearchResults<string> = await listScripts();
+    scripts.value = results.items;
+  } catch (e: unknown) {
+    emit("error", e);
+  }
+});
+</script>
+
+<template>
+  <AppSelect
+    floating
+    id="type"
+    label="languages.script.label"
+    :model-value="modelValue"
+    :options="options"
+    placeholder="languages.script.all"
+    validation="server"
+    @update:model-value="$emit('update:model-value', $event)"
+  />
+</template>
