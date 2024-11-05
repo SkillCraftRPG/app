9b3f252f3ec8c7c4742a659b7268ab4ad47d225b
Francis Pion <francispion@hotmail.com>
2024-11-04 23:26:13 -0500

    Implementing properties.

--- frontend/src/components/items/EquipmentProperties.vue
+++ frontend/src/components/items/EquipmentProperties.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import { TarCheckbox } from "logitar-vue3-ui";
+import { arrayUtils } from "logitar-js";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
 import DefenseInput from "./DefenseInput.vue";
 import ResistanceInput from "./ResistanceInput.vue";
-import type { EquipmentPropertiesModel } from "@/types/items";
+import type { EquipmentPropertiesModel, EquipmentTrait } from "@/types/items";
+
+const { rt, tm } = useI18n();
+
+type TranslatedTrait = {
+  value: EquipmentTrait;
+  text: string;
+};
 
 const props = defineProps<{
   modelValue: EquipmentPropertiesModel;
 }>();
+
+const traits = ref<TranslatedTrait[]>(
+  arrayUtils.orderBy(
+    Object.entries(tm(rt("items.equipment.traits"))).map(([value, text]) => ({ value, text }) as TranslatedTrait),
+    "text",
+  ),
+);
 
 const emit = defineEmits<{
   (e: "update:model-value", value: EquipmentPropertiesModel): void;
@@ -17,6 +36,18 @@ function setDefense(defense?: number): void {
 }
 function setResistance(resistance?: number): void {
   const properties: EquipmentPropertiesModel = { ...props.modelValue, resistance };
+  emit("update:model-value", properties);
+}
+
+function hasTrait(trait: EquipmentTrait): boolean {
+  return props.modelValue.traits.includes(trait);
+}
+function toggleTrait(trait: EquipmentTrait, add: boolean): void {
+  const properties: EquipmentPropertiesModel = { ...props.modelValue };
+  properties.traits = properties.traits.filter((t) => t !== trait);
+  if (add) {
+    properties.traits.push(trait);
+  }
   emit("update:model-value", properties);
 }
 </script>
@@ -31,6 +62,16 @@ function setResistance(resistance?: number): void {
         :model-value="modelValue.resistance"
         placeholder="items.equipment.resistance"
         @update:model-value="setResistance"
+      />
+    </div>
+    <div class="mb-3">
+      <TarCheckbox
+        v-for="trait in traits"
+        :key="trait.value"
+        :id="trait.value?.toLowerCase()"
+        :label="trait.text"
+        :model-value="hasTrait(trait.value)"
+        @update:model-value="toggleTrait(trait.value, $event)"
       />
     </div>
   </div>
--- frontend/src/components/items/WeaponProperties.vue
+++ frontend/src/components/items/WeaponProperties.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import { TarCheckbox } from "logitar-vue3-ui";
+import { arrayUtils } from "logitar-js";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
 import AttackInput from "./AttackInput.vue";
 import ResistanceInput from "./ResistanceInput.vue";
-import type { WeaponPropertiesModel } from "@/types/items";
+import type { WeaponPropertiesModel, WeaponTrait } from "@/types/items";
+
+const { rt, tm } = useI18n();
+
+type TranslatedTrait = {
+  value: WeaponTrait;
+  text: string;
+};
 
 const props = defineProps<{
   modelValue: WeaponPropertiesModel;
 }>();
+
+const traits = ref<TranslatedTrait[]>(
+  arrayUtils.orderBy(
+    Object.entries(tm(rt("items.weapon.traits"))).map(([value, text]) => ({ value, text }) as TranslatedTrait),
+    "text",
+  ),
+);
 
 const emit = defineEmits<{
   (e: "update:model-value", value: WeaponPropertiesModel): void;
@@ -17,6 +36,18 @@ function setAttack(attack?: number): void {
 }
 function setResistance(resistance?: number): void {
   const properties: WeaponPropertiesModel = { ...props.modelValue, resistance };
+  emit("update:model-value", properties);
+}
+
+function hasTrait(trait: WeaponTrait): boolean {
+  return props.modelValue.traits.includes(trait);
+}
+function toggleTrait(trait: WeaponTrait, add: boolean): void {
+  const properties: WeaponPropertiesModel = { ...props.modelValue };
+  properties.traits = properties.traits.filter((t) => t !== trait);
+  if (add) {
+    properties.traits.push(trait);
+  }
   emit("update:model-value", properties);
 }
 </script>
@@ -31,6 +62,16 @@ function setResistance(resistance?: number): void {
         :model-value="modelValue.resistance"
         placeholder="items.weapon.resistance"
         @update:model-value="setResistance"
+      />
+    </div>
+    <div class="mb-3">
+      <TarCheckbox
+        v-for="trait in traits"
+        :key="trait.value"
+        :id="trait.value?.toLowerCase()"
+        :label="trait.text"
+        :model-value="hasTrait(trait.value)"
+        @update:model-value="toggleTrait(trait.value, $event)"
       />
     </div>
   </div>
