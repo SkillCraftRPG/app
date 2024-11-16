d7a4c567fadf921dff93921dcb1f1f29961b70ca
Francis Pion <francispion@hotmail.com>
2024-11-16 01:00:53 -0500

    step2

--- frontend/src/components/characters/creation/Step2Personality.vue
+++ frontend/src/components/characters/creation/Step2Personality.vue
@@ -7,6 +7,7 @@ import { useI18n } from "vue-i18n";
 import PersonalitySelect from "@/components/personalities/PersonalitySelect.vue";
 import type { PersonalityModel } from "@/types/personalities";
 import type { Step2 } from "@/types/characters";
+import MarkdownText from "@/components/shared/MarkdownText.vue";
 
 const { t } = useI18n();
 
@@ -27,6 +28,7 @@ const onSubmit = handleSubmit(() => emit("continue", { personality: personality.
     <h3>{{ t("characters.steps.personality") }}</h3>
     <form @submit="onSubmit">
       <PersonalitySelect :model-value="personality?.id" required @error="$emit('error', $event)" @selected="personality = $event" />
+      <MarkdownText v-if="personality?.description" :text="personality.description" />
       <!-- TODO(fpion): Customizations -->
       <TarButton class="me-1" icon="fas fa-arrow-left" :text="t('actions.back')" variant="secondary" @click="$emit('back')" />
       <TarButton class="ms-1" icon="fas fa-arrow-right" :text="t('actions.continue')" type="submit" />
