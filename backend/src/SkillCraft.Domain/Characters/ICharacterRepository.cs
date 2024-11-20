18e4f39f3222661b64306e919ee0f36fd0cb1724
Francis Pion <francispion@hotmail.com>
2024-11-20 00:10:18 -0500

    repo

--- backend/src/SkillCraft.Domain/Characters/ICharacterRepository.cs
+++ backend/src/SkillCraft.Domain/Characters/ICharacterRepository.cs
@@ -2,6 +2,8 @@
 
 public interface ICharacterRepository
 {
+  Task<IReadOnlyCollection<Character>> LoadAsync(CancellationToken cancellationToken = default);
+
   Task SaveAsync(Character character, CancellationToken cancellationToken = default);
   Task SaveAsync(IEnumerable<Character> characters, CancellationToken cancellationToken = default);
 }
