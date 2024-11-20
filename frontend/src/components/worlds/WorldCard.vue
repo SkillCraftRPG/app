c425c59530c8342ee61793540f38b084737c2550
Francis Pion <francispion@hotmail.com>
2024-11-20 11:05:04 -0500

    Clickable WorldCard.

--- frontend/src/components/worlds/WorldCard.vue
+++ frontend/src/components/worlds/WorldCard.vue
@@ -17,12 +17,19 @@ defineEmits<{
 </script>
 
 <template>
-  <TarCard :title="world.name ?? world.slug" :subtitle="subtitle ? world.slug : undefined">
+  <TarCard class="card" :title="world.name ?? world.slug" :subtitle="subtitle ? world.slug : undefined" @click="$emit('entered')">
     <div class="my-2">
       {{ t("worlds.ownedBy") }}
       <TarAvatar :display-name="world.owner.displayName" :email-address="world.owner.emailAddress" icon="fas fa-user" :url="world.owner.pictureUrl" />
       {{ world.owner.displayName }}
     </div>
-    <TarButton class="float-end" icon="fas fa-dungeon" :text="t('actions.enter')" @click="$emit('entered')" />
+    <TarButton class="float-end" icon="fas fa-dungeon" :text="t('actions.enter')" />
   </TarCard>
 </template>
+
+<style scoped>
+.card:hover {
+  background-color: var(--bs-secondary-bg);
+  cursor: pointer;
+}
+</style>
