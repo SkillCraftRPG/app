f19e3e10068cddd47cd4c55669128f2fa4507c5e
Francis Pion <francispion@hotmail.com>
2024-11-21 18:54:40 -0500

    app

---
+++ backend/src/SkillCraft.Application/Characters/Validators/BonusValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using SkillCraft.Contracts.Characters;
+using SkillCraft.Domain.Validators;
+
+namespace SkillCraft.Application.Characters.Validators;
+
+internal class BonusValidator : AbstractValidator<BonusPayload>
+{
+  public BonusValidator()
+  {
+    RuleFor(x => x.Category).IsInEnum();
+    RuleFor(x => x.Target).BonusTarget();
+    RuleFor(x => x.Value).NotEqual(0);
+
+    When(x => !string.IsNullOrWhiteSpace(x.Precision), () => RuleFor(x => x.Precision!).Name());
+    When(x => !string.IsNullOrWhiteSpace(x.Notes), () => RuleFor(x => x.Notes!).Description());
+  }
+}
