e6feed03af85a879ebc8ebb13b5a52d22d122592
Francis Pion <francispion@hotmail.com>
2024-11-21 20:00:56 -0500

    domain

--- backend/tests/SkillCraft.Domain.UnitTests/Characters/CharacterTests.cs
+++ backend/tests/SkillCraft.Domain.UnitTests/Characters/CharacterTests.cs
@@ -1,4 +1,5 @@
 ﻿using SkillCraft.Contracts;
+using SkillCraft.Contracts.Characters;
 using SkillCraft.Contracts.Customizations;
 using SkillCraft.Domain.Aspects;
 using SkillCraft.Domain.Castes;
@@ -106,6 +107,21 @@ public class CharacterTests
       _caste,
       _education,
       _world.OwnerId);
+  }
+
+  [Fact(DisplayName = "AddBonus: it should add a new bonus.")]
+  public void AddBonus_it_should_add_a_new_bonus()
+  {
+    Assert.Empty(_character.Bonuses);
+
+    Bonus bonus = new(BonusCategory.Miscellaneous, MiscellaneousBonusTarget.Stamina.ToString(), value: 5, precision: new Name("Talent : Discipline"));
+    _character.AddBonus(bonus, _world.OwnerId);
+
+    KeyValuePair<Guid, Bonus> pair = Assert.Single(_character.Bonuses);
+    Assert.NotEqual(default, pair.Key);
+    Assert.Same(bonus, pair.Value);
+
+    Assert.Contains(_character.Changes, c => c is Character.BonusUpdatedEvent e && e.BonusId != default && e.Bonus.Equals(bonus));
   }
 
   [Fact(DisplayName = "AddItem: it should add a new item with options.")]
@@ -513,6 +529,39 @@ public class CharacterTests
     Assert.Equal("Weight", exception.ParamName);
   }
 
+  [Fact(DisplayName = "RemoveBonus: it should not do anything when the bonus was not found.")]
+  public void RemoveBonus_it_should_not_do_anything_when_the_bonus_was_not_found()
+  {
+    Bonus bonus = new(BonusCategory.Speed, SpeedKind.Swim.ToString(), value: 6);
+    _character.AddBonus(bonus, _world.OwnerId);
+
+    _character.ClearChanges();
+    Assert.False(_character.HasChanges);
+    Assert.Empty(_character.Changes);
+
+    _character.RemoveBonus(Guid.NewGuid(), _world.OwnerId);
+
+    _character.ClearChanges();
+    Assert.False(_character.HasChanges);
+    Assert.Empty(_character.Changes);
+
+    Assert.Same(bonus, Assert.Single(_character.Bonuses).Value);
+  }
+
+  [Fact(DisplayName = "RemoveBonus: it should remove an bonus language.")]
+  public void RemoveBonus_it_should_remove_an_existing_bonus()
+  {
+    Guid bonusId = Guid.NewGuid();
+    Bonus bonus = new(BonusCategory.Miscellaneous, MiscellaneousBonusTarget.Dodge.ToString(), value: +2);
+    _character.SetBonus(bonusId, bonus, _world.OwnerId);
+    Assert.NotEmpty(_character.Bonuses);
+
+    _character.RemoveBonus(bonusId, _world.OwnerId);
+    Assert.Empty(_character.Bonuses);
+
+    Assert.Contains(_character.Changes, change => change is Character.BonusRemovedEvent e && e.BonusId == bonusId);
+  }
+
   [Fact(DisplayName = "RemoveLanguage: it should not do anything when the language was not found.")]
   public void RemoveLanguage_it_should_not_do_anything_when_the_language_was_not_found()
   {
@@ -553,6 +602,60 @@ public class CharacterTests
     Assert.Empty(_character.Talents);
 
     Assert.Contains(_character.Changes, change => change is Character.TalentRemovedEvent e && e.RelationId == id);
+  }
+
+  [Fact(DisplayName = "SetBonus: it should not do anything when the bonus has not changed.")]
+  public void SetBonus_it_should_not_do_anything_when_the_bonus_has_not_changed()
+  {
+    Assert.Empty(_character.Bonuses);
+
+    Guid bonusId = Guid.NewGuid();
+    Bonus bonus = new(BonusCategory.Statistic, Statistic.Learning.ToString(), value: +4);
+    _character.SetBonus(bonusId, bonus, _world.OwnerId);
+
+    _character.ClearChanges();
+    Assert.False(_character.HasChanges);
+    Assert.Empty(_character.Changes);
+
+    _character.SetBonus(bonusId, bonus, _world.OwnerId);
+    Assert.False(_character.HasChanges);
+    Assert.Empty(_character.Changes);
+  }
+
+  [Fact(DisplayName = "SetBonus: it should replace an existing bonus.")]
+  public void SetBonus_it_should_replace_an_existing_bonus()
+  {
+    Assert.Empty(_character.Bonuses);
+
+    Guid bonusId = Guid.NewGuid();
+
+    Bonus oldBonus = new(BonusCategory.Skill, Skill.Performance.ToString(), value: +1);
+    _character.SetBonus(bonusId, oldBonus, _world.OwnerId);
+    Assert.Contains(_character.Changes, c => c is Character.BonusUpdatedEvent e && e.BonusId == bonusId && e.Bonus.Equals(oldBonus));
+
+    Bonus newBonus = new(BonusCategory.Skill, Skill.Perception.ToString(), value: +2, precision: new Name("Hat of Performance"));
+    _character.SetBonus(bonusId, newBonus, _world.OwnerId);
+    Assert.Contains(_character.Changes, c => c is Character.BonusUpdatedEvent e && e.BonusId == bonusId && e.Bonus.Equals(newBonus));
+
+    KeyValuePair<Guid, Bonus> pair = Assert.Single(_character.Bonuses);
+    Assert.Equal(bonusId, pair.Key);
+    Assert.Same(newBonus, pair.Value);
+  }
+
+  [Fact(DisplayName = "SetBonus: it should set a new bonus.")]
+  public void SetBonus_it_should_set_a_new_bonus()
+  {
+    Assert.Empty(_character.Bonuses);
+
+    Guid bonusId = Guid.NewGuid();
+    Bonus bonus = new(BonusCategory.Miscellaneous, MiscellaneousBonusTarget.Stamina.ToString(), value: 5, precision: new Name("Talent : Discipline"));
+    _character.SetBonus(bonusId, bonus, _world.OwnerId);
+
+    KeyValuePair<Guid, Bonus> pair = Assert.Single(_character.Bonuses);
+    Assert.Equal(bonusId, pair.Key);
+    Assert.Same(bonus, pair.Value);
+
+    Assert.Contains(_character.Changes, c => c is Character.BonusUpdatedEvent e && e.BonusId == bonusId && e.Bonus.Equals(bonus));
   }
 
   [Fact(DisplayName = "SetItem: it should add a new item.")]
