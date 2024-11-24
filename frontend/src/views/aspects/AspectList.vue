dcfa0d72a7dc1f928406c9111f2948ec9b2706bc
Francis Pion <francispion@hotmail.com>
2024-11-23 23:29:15 -0500

    Revert "Fixed active tab. (#218)"
    This reverts commit 2889aafefc2d8518e0dc18ed843e013ae9d2d6f1.

--- frontend/src/views/aspects/AspectList.vue
+++ frontend/src/views/aspects/AspectList.vue
@@ -162,7 +162,7 @@ watch(
         :model-value="sort"
         :options="sortOptions"
         @descending="setQuery('isDescending', $event.toString())"
-        @update:model-value="setQuery('sort', $event ?? '')"
+        @update:model-value="setQuery('sort', $eveant ?? '')"
       />
       <CountSelect class="col-lg-4" :model-value="count" @update:model-value="setQuery('count', ($event ?? 10).toString())" />
     </div>
