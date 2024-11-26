f99772bae6e6043479e1b4f65f91fe8fe8c603c6
Francis Pion <francispion@hotmail.com>
2024-11-25 22:56:52 -0500

    Calculate character speeds. (#233)

---
+++ backend/src/SkillCraft.Domain/Characters/CharacterSpeeds.cs
@@ -0,0 +1,42 @@
+﻿using SkillCraft.Contracts;
+using SkillCraft.Contracts.Lineages;
+
+namespace SkillCraft.Domain.Characters;
+
+public record CharacterSpeeds : ISpeeds
+{
+  public int Walk { get; }
+  public int Climb { get; }
+  public int Swim { get; }
+  public int Fly { get; }
+  public int Hover { get; }
+  public int Burrow { get; }
+
+  public CharacterSpeeds(IEnumerable<KeyValuePair<SpeedKind, int>> speeds)
+  {
+    foreach (KeyValuePair<SpeedKind, int> speed in speeds)
+    {
+      switch (speed.Key)
+      {
+        case SpeedKind.Burrow:
+          Burrow = speed.Value;
+          break;
+        case SpeedKind.Climb:
+          Climb = speed.Value;
+          break;
+        case SpeedKind.Fly:
+          Fly = speed.Value;
+          break;
+        case SpeedKind.Hover:
+          Hover = speed.Value;
+          break;
+        case SpeedKind.Swim:
+          Swim = speed.Value;
+          break;
+        case SpeedKind.Walk:
+          Walk = speed.Value;
+          break;
+      }
+    }
+  }
+}
