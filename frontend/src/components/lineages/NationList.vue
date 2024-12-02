5fcf9ec77027d09d96267c6554038a50228da277
Francis Pion <francispion@hotmail.com>
2024-12-02 00:51:14 -0500

    Fixed NationCard style & behavior. (#265)

--- frontend/src/components/lineages/NationList.vue
+++ frontend/src/components/lineages/NationList.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { TarCard } from "logitar-vue3-ui";
 import { onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 import CreateLineage from "./CreateLineage.vue";
+import LineageCard from "./LineageCard.vue";
 import type { LineageModel, SearchLineagesPayload } from "@/types/lineages";
 import type { SearchResults } from "@/types/search";
 import { searchLineages } from "@/api/lineages";
@@ -53,13 +53,7 @@ onMounted(async () => {
     </div>
     <div v-if="nations.length > 0" class="mb-3 row">
       <div v-for="nation in nations" :key="nation.id" class="col-lg-3">
-        <TarCard :title="nation.name">
-          <div class="float-end">
-            <RouterLink class="btn btn-primary" :to="{ name: 'LineageEdit', params: { id: nation.id } }">
-              <font-awesome-icon icon="fas fa-edit" /> {{ t("actions.edit") }}
-            </RouterLink>
-          </div>
-        </TarCard>
+        <LineageCard :lineage="nation" />
       </div>
     </div>
     <p v-else>{{ t("lineages.nations.empty") }}</p>
