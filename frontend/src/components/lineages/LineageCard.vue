658ac28c2c692cbb7934c10bbceaa6d53388b5a3
Francis Pion <francispion@hotmail.com>
2024-12-02 00:50:40 -0500

    Fixed NationCard style & behavior.

---
+++ frontend/src/components/lineages/LineageCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { TarCard } from "logitar-vue3-ui";
+import { useRouter } from "vue-router";
+
+import type { LineageModel } from "@/types/lineages";
+
+const router = useRouter();
+
+const props = defineProps<{
+  lineage: LineageModel;
+}>();
+
+function onClick(): void {
+  router.push({ name: "LineageEdit", params: { id: props.lineage.id } });
+}
+</script>
+
+<template>
+  <TarCard class="clickable" :title="lineage.name" @click="onClick" />
+</template>
+
+<style scoped>
+.clickable:hover {
+  background-color: var(--bs-secondary-bg);
+  cursor: pointer;
+}
+</style>
