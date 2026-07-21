4e2e9dde747e4bc056e8f425d72d29205a638aae
Francis Pion <francispion@hotmail.com>
2026-07-21 02:07:30 -0400

    fixes

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_API_BASE_URL?: string;
   readonly VITE_APP_ASSETS_BASE_URL?: string;
+  readonly VITE_APP_SWAGGER_ENABLED?: boolean;
   readonly VITE_APP_VERSION: string;
 }
 
--- src/fontawesome.ts
+++ src/fontawesome.ts
@@ -2,9 +2,9 @@ import type { App } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faAdjust, faArrowUp, faHome, faMoon, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faAdjust, faArrowUp, faHome, faMoon, faSun, faUser, faVial } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faAdjust, faArrowUp, faHome, faMoon, faSun, faUser);
+library.add(faAdjust, faArrowUp, faHome, faMoon, faSun, faUser, faVial);
 
 export default function (app: App) {
   app.component("font-awesome-icon", FontAwesomeIcon);
