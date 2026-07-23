d00e918cfc2c3758b0bc7b047e2c1581e159955c
Francis Pion <francispion@hotmail.com>
2026-07-23 10:08:26 -0400

    Sign-Out Flow. (#7)

--- src/views/account/SignOutView.vue
+++ src/views/account/SignOutView.vue
@@ -1,5 +1,28 @@
 <template>
-  <main class="container"></main>
+  <div></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject, onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+import { handleErrorKey } from "@/inject";
+import { signOut } from "@/api/account";
+import { useAccountStore } from "@/stores/account";
+
+const account = useAccountStore();
+const handleError = inject(handleErrorKey) as (e: unknown) => void;
+const router = useRouter();
+
+onMounted(async () => {
+  if (account.currentUser) {
+    try {
+      await signOut();
+      account.signOut();
+    } catch (e: unknown) {
+      handleError(e);
+    }
+  }
+  router.push({ name: "SignIn" });
+});
+</script>
