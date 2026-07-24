e03065b0d371fc10be3b533a688757d2bb293047
Francis Pion <francispion@hotmail.com>
2026-07-23 23:57:51 -0400

    msg

--- src/api/sessions.ts
+++ src/api/sessions.ts
@@ -8,3 +8,9 @@ export async function listSessions(): Promise<SearchResults<Session>> {
   const url: string = new urlUtils.UrlBuilder({ path: "/sessions" }).buildRelative();
   return (await get<SearchResults<Session>>(url)).data;
 }
+
+// Déconnecter toutes les sessions ?
+// Vous serez déconnecté de tous vos appareils, y compris celui-ci. Vous devrez vous connecter de nouveau pour accéder à votre compte.
+
+// Sign out of all sessions?
+// You’ll be signed out on all your devices, including this one. You’ll need to sign in again to access your account.
