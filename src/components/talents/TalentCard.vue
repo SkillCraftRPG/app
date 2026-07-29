af773c732e51a2ad40b7c0ae154bac66d73325e5
Francis Pion <francispion@hotmail.com>
2026-07-29 11:39:51 -0400

    TalentCard

---
+++ src/components/talents/TalentCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <LinkCard :to="{ name: 'Talent', params: { id: talent.id } }">
+    <template #title-override>
+      <div class="d-flex flex-wrap align-items-center gap-2">
+        <h5 class="card-title">{{ talent.name }}</h5>
+        <TarBadge variant="secondary">{{ talent.tier }}</TarBadge>
+      </div>
+    </template>
+    <template v-if="talent.requiredTalent" #subtitle-override>
+      <h6 class="card-subtitle mb-2 text-body-secondary">
+        <font-awesome-icon icon="fas fa-code-branch" aria-hidden="true" />&nbsp;{{ talent.requiredTalent.name }}
+      </h6>
+    </template>
+    <div v-if="talent.skill || talent.allowMultiplePurchases" class="card-text d-flex justify-content-between align-items-center gap-2 mb-2">
+      <div class="text-start">
+        <template v-if="talent.skill">
+          <font-awesome-icon icon="fas fa-kitchen-set" aria-hidden="true" />&nbsp;{{ t(`game.skill.options.${talent.skill}`) }}
+        </template>
+      </div>
+      <div v-if="talent.allowMultiplePurchases" class="text-end">
+        <TarBadge pill variant="secondary"><font-awesome-icon icon="fas fa-tag" aria-hidden="true" />&nbsp;{{ t("talents.allowMultiplePurchases") }}</TarBadge>
+      </div>
+    </div>
+    <div v-if="talent.summary" class="card-text">{{ talent.summary }}</div>
+    <StatusBlock :actor="talent.updatedBy" class="card-text mt-2 small text-secondary" :date="talent.updatedOn" relative />
+  </LinkCard>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+import LinkCard from "@/components/shared/LinkCard.vue";
+import StatusBlock from "@/components/shared/StatusBlock.vue";
+import TarBadge from "@/components/tar/TarBadge.vue";
+import type { Talent } from "@/types/talents";
+
+const { t } = useI18n();
+
+defineProps<{
+  talent: Talent;
+}>();
+</script>
