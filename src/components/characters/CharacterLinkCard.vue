7bf4497e5c3f4153d92d2ba02b3ee2d85c1311a7
Francis Pion <francispion@hotmail.com>
2026-08-13 23:50:30 -0400

    list

---
+++ src/components/characters/CharacterLinkCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <LinkCard :title="character.name" :to="{ name: 'Character', params: { id: character.id } }">
+    <StatusBlock :actor="character.updatedBy" class="card-text mt-2 small text-secondary" :date="character.updatedOn" relative />
+  </LinkCard>
+</template>
+
+<script setup lang="ts">
+import LinkCard from "@/components/shared/LinkCard.vue";
+import StatusBlock from "@/components/shared/StatusBlock.vue";
+import type { Character } from "@/types/characters";
+
+defineProps<{
+  character: Character;
+}>();
+</script>
