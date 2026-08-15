01bcec12aa2427bc189cf98cd16f9a1efe9a7848
Francis Pion <francispion@hotmail.com>
2026-08-15 00:35:10 -0400

    lineage

--- src/components/characters/header/CharacterLineage.vue
+++ src/components/characters/header/CharacterLineage.vue
@@ -20,8 +20,37 @@
       </div>
       <div v-if="target.summary" class="fst-italic text-body-secondary mb-3">{{ target.summary }}</div>
       <MarkdownContent v-if="target.content" class="mb-3" :text="target.content"></MarkdownContent>
-      <!-- TODO(fpion): Features -->
-      <!-- TODO(fpion): Speeds -->
+      <template v-if="hasSpeeds">
+        <div class="fs-5 mb-1">{{ t("lineages.physical.speeds.lead") }}</div>
+        <div class="row text-center">
+          <div v-if="target.speeds.walk" class="col-4 col-md-fifth mb-3">
+            <div class="fw-bold">{{ t("game.speed.kind.options.Walk") }}</div>
+            <div>{{ target.speeds.walk }}</div>
+          </div>
+          <div v-if="target.speeds.climb" class="col-4 col-md-fifth mb-3">
+            <div class="fw-bold">{{ t("game.speed.kind.options.Climb") }}</div>
+            <div>{{ target.speeds.climb }}</div>
+          </div>
+          <div v-if="target.speeds.swim" class="col-4 col-md-fifth mb-3">
+            <div class="fw-bold">{{ t("game.speed.kind.options.Swim") }}</div>
+            <div>{{ target.speeds.swim }}</div>
+          </div>
+          <div v-if="target.speeds.fly" class="col-4 col-md-fifth mb-3">
+            <div class="fw-bold">{{ t(`game.speed.${target.speeds.hover ? "hover" : "kind.options.Fly"}`) }}</div>
+            <div>{{ target.speeds.fly }}</div>
+          </div>
+          <div v-if="target.speeds.burrow" class="col-4 col-md-fifth mb-3">
+            <div class="fw-bold">{{ t("game.speed.kind.options.Burrow") }}</div>
+            <div>{{ target.speeds.burrow }}</div>
+          </div>
+        </div>
+      </template>
+      <template v-if="features.length">
+        <template v-for="(feature, index) in features" :key="index">
+          <div class="fs-5 mb-1">{{ t("game.feature.format", { name: feature.name }) }}</div>
+          <MarkdownContent v-if="feature.content" class="mb-3" :text="feature.content"></MarkdownContent>
+        </template>
+      </template>
       <template #footer>
         <TarButton icon="fas fa-xmark" :text="t('actions.close')" variant="secondary" @click="close" />
       </template>
@@ -30,6 +59,7 @@
 </template>
 
 <script setup lang="ts">
+import { arrayUtils } from "logitar-js";
 import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 
@@ -38,7 +68,9 @@ import TarButton from "@/components/tar/TarButton.vue";
 import TarCard from "@/components/tar/TarCard.vue";
 import TarModal from "@/components/tar/TarModal.vue";
 import type { Lineage } from "@/types/lineages";
+import type { Feature } from "@/types/features";
 
+const { orderBy } = arrayUtils;
 const { t } = useI18n();
 
 const props = defineProps<{
@@ -51,6 +83,10 @@ const selected = ref<"ethnicity" | "species">("species");
 const ethnicity = computed<Lineage | undefined>(() => (props.lineage.parent ? props.lineage : undefined));
 const species = computed<Lineage>(() => props.lineage.parent ?? props.lineage);
 const target = computed<Lineage>(() => (selected.value === "ethnicity" && ethnicity.value ? ethnicity.value : species.value));
+const features = computed<Feature[]>(() => orderBy(target.value.features, "name"));
+const hasSpeeds = computed<boolean>(() =>
+  Boolean(target.value.speeds.walk || target.value.speeds.climb || target.value.speeds.swim || target.value.speeds.fly || target.value.speeds.burrow),
+);
 
 function close(): void {
   modal.value?.hide();
