889c8fbd7220e28cee8920a354126ca704a2066b
Francis Pion <francispion@hotmail.com>
2026-08-16 18:20:58 -0400

    fix

--- src/components/characters/CharacterOverview.vue
+++ src/components/characters/CharacterOverview.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <CharacterHeader :character="character" />
     <div class="row">
       <div class="col-md-4 mb-3">
         <CharacterLineage :lineage="character.lineage" />
