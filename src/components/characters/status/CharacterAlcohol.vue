08fcb926b9283fa06eaed384338d6c9789a8959b
Francis Pion <francispion@hotmail.com>
2026-08-24 15:37:39 -0400

    Fixed reinitialize. (#67)

--- src/components/characters/status/CharacterAlcohol.vue
+++ src/components/characters/status/CharacterAlcohol.vue
@@ -36,7 +36,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed, nextTick, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 import AlcoholField from "./AlcoholField.vue";
@@ -107,7 +107,7 @@ watch(
   () => props.character,
   (character) => {
     current.value = character.bloodAlcoholContent;
-    reinitialize();
+    nextTick(reinitialize);
   },
   { deep: true, immediate: true },
 );
