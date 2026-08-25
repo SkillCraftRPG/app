eb1e18eaa6c988075f42246a27058283b0efa9b7
Francis Pion <francispion@hotmail.com>
2026-08-24 20:51:02 -0400

    Refactored import cards.

--- src/components/talents/TalentImportCard.vue
+++ src/components/talents/TalentImportCard.vue
@@ -1,26 +1,19 @@
 <template>
-  <TalentCard :clickable="isClickable" :selected="selected" :talent="talent">
-    <div class="d-flex justify-content-between mt-2">
-      <div>
-        <font-awesome-icon v-if="icon" :icon="icon" />
-      </div>
-      <div>
-        <ImportStatusDisplay :status="status" />
-      </div>
+  <TalentCard :clickable="isClickable" :selected="selected" :selection="selection" :talent="talent">
+    <div class="mt-2 text-end">
+      <ImportStatusDisplay :status="status" />
     </div>
   </TalentCard>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
-import { parsingUtils } from "logitar-js";
 
 import ImportStatusDisplay from "@/components/import/ImportStatusDisplay.vue";
 import TalentCard from "@/components/talents/TalentCard.vue";
 import type { ImportStatus } from "@/types/import";
+import type { SelectionKind } from "@/types/shared";
 import type { Talent } from "@/types/talents";
-
-const { parseBoolean } = parsingUtils;
 
 const props = defineProps<{
   selected: boolean | string;
@@ -29,9 +22,5 @@ const props = defineProps<{
 }>();
 
 const isClickable = computed<boolean>(() => props.status !== "UpToDate");
-const icon = computed<string | undefined>(() => {
-  if (isClickable.value) {
-    return parseBoolean(props.selected) ? "far fa-square-check" : "far fa-square";
-  }
-});
+const selection = computed<SelectionKind | undefined>(() => (isClickable.value ? "multiple" : undefined));
 </script>
